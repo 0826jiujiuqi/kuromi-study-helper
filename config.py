ff0b9b8f38efe515f6f3e0ae073374dd80ad4a0f
0826jiujiuqi <1728638930@qq.com>
2026-07-13 15:19:21 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
-DB_URL = os.environ.get("LIBSQL_URL")
-DB_TOKEN = os.environ.get("LIBSQL_TOKEN")
+CF_ACCOUNT_ID = os.environ.get("CF_ACCOUNT_ID")
+CF_API_TOKEN = os.environ.get("CF_API_TOKEN")
+CF_D1_DB_ID = os.environ.get("CF_D1_DB_ID")
 SUBJECTS = ['数学','英语','专业课','政治']
 EXAM_DATE = '2026-12-20'
 DEFAULT_GOALS = {'数学':{'一轮结束':'2026-08-31','刷题开始':'2026-09-15'},'英语':{'一轮结束':'2026-08-20','刷题开始':'2026-09-01'},'专业课':{'一轮结束':'2026-09-10','刷题开始':'2026-09-20'},'政治':{'一轮结束':'2026-09-30','刷题开始':'2026-10-10'}}
